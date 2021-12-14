3ffd94730ba1b279ab01c8900b527163d3a0dea9
dcasanovasos <74675769+dcasanovasos@users.noreply.github.com>
2021-12-14 11:54:05 +0100

    Create 07.ps1
    Versió Major de PowerShell

---
+++ 07.ps1
@@ -0,0 +1,4 @@
+#Si la versio de PowerShell fos 6.2.0, la part Major de la versió seria 6. 
+#Obtingues el número de la versió Major de PowerShell (és possible que hagis de fer una doble pipe). Mostra'l per pantalla.
+$a=PSVersionTable | Select-Object -ExpandProperty PSVersion| Select-Object -ExpandProperty  Major
+Write-Output $a
