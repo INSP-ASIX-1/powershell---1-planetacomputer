596f1ec4d13bb60a9207de4accd21c16a4b9a5e0
dcasanovasos <74675769+dcasanovasos@users.noreply.github.com>
2021-12-22 11:28:02 +0100

    Create 09.ps1
    si dos numeros passats per parametre a l'script son iguals mostra OK. Si son diferents mostra KO

---
+++ 09.ps1
@@ -0,0 +1,7 @@
+#si dos numeros passats per parametre a l'script son iguals mostra OK. Si son diferents mostra KO
+if ($args[0] -eq $args[1]){
+        Write-Output "OK"
+}
+else{
+    Write-Output "KO"
+}
